ede059a1532a657ea053b3160f3a805c919aadab
ccheshire519 <CandT@outlook.com>
2025-11-18 19:37:15 +0000

    Added Question and QuizSystem classes

---
+++ Question.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Question
+{
+	public Question()
+	{
+		private int questionID;
+		private string questionText;
+		private string questionCorrectAnswer;
+		private string questionDifficulty;
+
+		List<string> QuestionOptions = new List<string>()
+		{
+
+		};
+
+			public int QuestionID
+			{
+					get { return questionID; }
+					set { questionID = value; }
+			}
+			public string QuestionText
+			{
+					get { return questionText; }
+					set { questionText = value; }
+			}
+			public string QuestionCorrectAnswer
+			{
+					get { return questionCorrectAnswer; }
+					set { questionCorrectAnswer = value; }
+			}
+			public string QuestionDifficulty
+			{
+					get { return questionDifficulty; }
+					set { questionDifficulty = value; }
+			}
+			// Constructor
+			public Question(int questionID, string questionText, string questionCorrectAnswer, string questionDifficulty)
+			{
+					this.questionID = questionID;
+					this.questionText = questionText;
+					this.questionCorrectAnswer = questionCorrectAnswer;
+					this.questionDifficulty = questionDifficulty;
+			}
+}
+}
---
+++ QuizSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class QuizSystem
+{
+	public QuizSystem()
+	{
+		List<string> AdminUsers = new List<string>()
+		{
+			
+		};
+        List<string> StudentUsers = new List<string>()
+        {
+
+        };
+        List<string> Quiz = new List<string>()
+        {
+
+        };
+        List<string> Categories = new List<string>()
+        {
+
+        };
+    }
+}
