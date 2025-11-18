7ac85fd30d2bb4b8617689a49ae412e968704193
ccheshire519 <CandT@outlook.com>
2025-11-18 12:12:29 +0000

    Added User class with private fields, gets and sets and public user constructor

---
+++ User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class User
+{
+	public User()
+	{
+        // constructor
+        private int id;
+		private string username;
+		private string email;
+		private string password;
+		private string role;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+		public string Username
+		{
+			get { return username; }
+			set { username = value; }
+		}
+		public string Email
+		{
+			get { return email; }
+			set { email = value; }
+		}
+		public string Password
+		{
+			get { return password; }
+			set { password = value; }
+		}
+		public string Role
+		{
+			get { return role; }
+			set { role = value; }
+		}
+
+		public User(int id, string username, string email, string password, string role)
+		{
+			this.id = id;
+			this.username = username;
+			this.email = email;
+			this.password = password;
+			this.role = role;
+		}
+		
+		public Logout()
+		{
+			Console.WriteLine("User logged out."); 
+		}
+
+		public UpdateProfile()
+		{		
+			Console.WriteLine("User profile updated.");
+		}
+	}
+}
