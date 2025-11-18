785058398f3ad492bba4a288151659f62a186a7c
ccheshire519 <CandT@outlook.com>
2025-11-18 19:26:44 +0000

    Quiz and Category classes added

---
+++ Quiz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Quiz
+{
+	public Quiz()
+	{
+		private int QuizID;
+		private string QuizTitle;
+		private	string QuizDescription;
+		private DateTime QuizDate;
+
+			public int QuizID
+			{
+					get { return QuizID; }
+					set { QuizID = value; }
+			}
+			public string QuizTitle
+			{
+					get { return QuizTitle; }
+					set { QuizTitle = value; }
+			}
+			public string QuizDescription
+			{
+					get { return QuizDescription; }
+					set { QuizDescription = value; }
+			}
+			public DateTime QuizDate
+			{
+					get { return QuizDate; }
+					set { QuizDate = value; }
+			}
+			// Constructor
+			public Quiz(int quizID, string quizTitle, string quizDescription, DateTime quizDate)
+			{
+					this.QuizID = quizID;
+					this.QuizTitle = quizTitle;
+					this.QuizDescription = quizDescription;
+					this.QuizDate = quizDate;
+			}
+}
+}
