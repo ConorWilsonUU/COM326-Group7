cbf8c0542286359715b52e1c96a1ffbdd0e7a2db
ccheshire519 <CandT@outlook.com>
2025-11-18 19:21:34 +0000

    Admin and Student Classes

---
+++ Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Admin: User
+{
+	public Admin()
+	{
+		private DateTime loginDate;
+
+		public DateTime LoginDate
+		{
+				get { return loginDate; }
+				set { loginDate = value; }
+		}
+    // Constructor
+    public Admin(string username, string password, DateTime loginDate): base(username, password)
+		{
+			this.loginDate = loginDate;
+		}
+	}
+}
